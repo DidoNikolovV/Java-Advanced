a179fdd66411abfb19f0395ff369346af2aa9b50
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-10-26 17:55:10 +0300

    RecursiveFibonacci exercise solution

---
+++ Stacks-Queues/src/RecursiveFibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+    public static long[] memory; // saves already calculated fibonacci numbers
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        memory = new long[n + 1];
+
+        System.out.println(fibonacci(n));
+    }
+
+    public static long fibonacci(int n) {
+        if(n < 2) {
+            return 1;
+        }
+
+        if(memory[n] != 0) {
+            return memory[n];
+        }
+
+        memory[n] = fibonacci(n -1) + fibonacci(n - 2);
+        return memory[n];
+    };
+}
