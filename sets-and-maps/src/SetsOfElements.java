7dcc72f653870afdb180a48a3bd21dbbe0e6f503
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-11-02 22:43:03 +0200

    SetsOfElements exercise solution

---
+++ sets-and-maps/src/SetsOfElements.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class SetsOfElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] input = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int n = input[0];
+        int m = input[1];
+        int totalCount = n + m;
+
+        Set<Integer> nSet = new LinkedHashSet<>();
+        Set<Integer> mSet = new LinkedHashSet<>();
+
+        for(int i = 0; i < totalCount; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+
+            if(i < n) {
+                nSet.add(number);
+            } else {
+                mSet.add(number);
+            }
+        }
+
+        Set<Integer> repeatingElements = new LinkedHashSet<>();
+
+        if(nSet.size() > mSet.size()) {
+            for(int num : nSet) {
+                if (mSet.contains(num)) {
+                    repeatingElements.add(num);
+                }
+            }
+        } else {
+            for(int num : mSet) {
+                if(nSet.contains(num)) {
+                    repeatingElements.add(num);
+                }
+            }
+        }
+    }
+
+
+
+    }
+}
