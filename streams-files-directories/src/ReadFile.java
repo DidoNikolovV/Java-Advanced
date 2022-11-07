3e34055656170878c5b782108ac8ca9f96bea4e1
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-11-07 14:26:14 +0200

    ReadFile exercise solution

---
+++ streams-files-directories/src/ReadFile.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String path = "input.txt";
+        try {
+            FileInputStream fileStream = new FileInputStream(path);
+            int oneByte = fileStream.read();
+            while(oneByte > 0) {
+                System.out.printf("%s ", Integer.toBinaryString(oneByte));
+                oneByte = fileStream.read();
+            }
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
