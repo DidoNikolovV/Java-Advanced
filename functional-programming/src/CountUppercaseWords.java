9026d7e2e4dc1808db53b20339d181f9404230fe
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-11-09 08:45:34 +0200

    CountUppercaseWords exercise solution

---
+++ functional-programming/src/CountUppercaseWords.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+
+public class CountUppercaseWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Predicate<String> startsWithUppercase = w -> Character.isUpperCase(w.charAt(0));
+
+        List<String> uppercaseWords = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(startsWithUppercase)
+                .collect(Collectors.toList());
+
+        System.out.println(uppercaseWords.size());
+
+        System.out.println(String.join(System.lineSeparator(), uppercaseWords));
+
+
+    }
+
+}
