a16518119e6f912b0832bab51f58259bbb1f001a
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-11-09 19:04:02 +0200

    KnightsOfHonor exercise solution

---
+++ functional-programming/src/KnightsOfHonor.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class KnightsOfHonor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Consumer<String> addSir = n -> System.out.println("Sir " + n);
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .forEach(addSir);
+    }
+}
