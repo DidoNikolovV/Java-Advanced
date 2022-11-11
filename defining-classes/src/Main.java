0efd44978ec3e7c2712de4c6777c024f5b88e816
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-11-11 17:13:24 +0200

    OpinionPoll exercise solution

--- defining-classes/src/Main.java
+++ defining-classes/src/Main.java
@@ -1,59 +1,33 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String input = scanner.nextLine();
+        int n = Integer.parseInt(scanner.nextLine());
 
-        Map<Integer, BankAccount> bankAccounts = new HashMap<>();
+//        Map<String, Integer> people = new TreeMap<>();
+        List<Person> people = new ArrayList<>();
 
+        int ageRestriction = 30;
 
-        while(!input.equals("End")) {
-            String[] inputParts = input.split(" ");
-            String command = inputParts[0];
-            String output = null;
-            switch(command) {
-                case "Create":
-                    int id = BankAccount.getId();
-                    BankAccount bankAccount = new BankAccount();
-                    bankAccounts.put(id, bankAccount);
-                    output = "Account ID" + id + " created";
-                    break;
-                case "Deposit":
-                    BankAccount account = bankAccounts.get(Integer.parseInt(inputParts[1]));
-                    if(account == null) {
-                        output = "Account does not exist";
-                    } else {
-                        int amount = Integer.parseInt(inputParts[2]);
-                        account.deposit(amount);
-                        output = "Deposited " + amount + " to " + "ID" + inputParts[1];
-                    }
-                    break;
-                case "SetInterest":
-                        double newInterest = Double.parseDouble(inputParts[1]);
-                        BankAccount.setInterestRate(newInterest);
-                    break;
-                case "GetInterest":
-                    BankAccount accountToGetInterest = bankAccounts.get(Integer.parseInt(inputParts[1]));
-                    if(accountToGetInterest == null) {
-                        output = "Account does not exist";
-                    } else {
-                        double interest = accountToGetInterest.getInterest(Integer.parseInt(inputParts[2]));
-                        output = String.format("%.2f", interest);
-                    }
-                    break;
+        while(n-- > 0) {
+            String[] inputParts = scanner.nextLine().split(" ");
+            String name = inputParts[0];
+            int age = Integer.parseInt(inputParts[1]);
 
-            }
-
-            if(output != null) {
-                System.out.println(output);
-
-            }
-            input = scanner.nextLine();
+            Person person = new Person(name, age);
+            people.add(person);
         }
+
+        people = people.stream().filter(p -> p.getAge() > ageRestriction).sorted((a, b) -> a.getName().compareTo(b.getName())).collect(Collectors.toList());
+        for(var p: people) {
+            System.out.println(p.getName() + " - " + p.getAge());
+        }
+
 
     }
 }
