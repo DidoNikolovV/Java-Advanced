b4bcfad03e1501a788cc02bbba340f14462adbe2
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-11-17 09:44:19 +0200

    Implement remove method

--- workshop/src/SmartArray.java
+++ workshop/src/SmartArray.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 public class SmartArray {
-    private int[] data;
+    private Integer[] data;
     private int size;
 
     public SmartArray() {
-        this.data = new int[4];
+        this.data = new Integer[4];
         this.size = 0;
     }
 
@@ -18,18 +18,35 @@ public class SmartArray {
     }
 
     public int get(int index) {
+        validateIndex(index);
+        return this.data[index];
+    }
+
+    private void validateIndex(int index) {
         if(index < 0 || index >= this.size) {
             throw new IndexOutOfBoundsException("Invalid index " + index);
         }
-        return this.data[index];
     }
 
     public int size() {
         return this.size;
     }
 
-    private int[] grow() {
-        int[] newData = new int[this.data.length * 2];
+    public int remove(int index) {
+        validateIndex(index);
+        int element = this.data[index];
+        this.size--;
+
+        for(int i = index ; i < this.size; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+
+        this.data[this.size] = null;
+        return element;
+    }
+
+    private Integer[] grow() {
+        Integer[] newData = new Integer[this.data.length * 2];
 
         System.arraycopy(this.data, 0, newData, 0, this.data.length);
 //        for(int i = 0; i < this.data.length; i++) {
