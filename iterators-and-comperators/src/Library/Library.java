4b5f6aba9f4c8175964cc5eec74bd52f0c550556
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-11-22 17:26:28 +0200

    Create Library that holds array of books and its Iterable

---
+++ iterators-and-comperators/src/Library/Library.java
@@ -0,0 +1,43 @@
+package Library;
+
+import java.util.Iterator;
+
+public class Library implements Iterable<Book> {
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    public class LibIterator implements Iterator<Book> {
+        private int counter = 0;
+
+        @Override
+        public boolean hasNext() {
+            return counter < books.length;
+        }
+
+        @Override
+        public Book next() {
+            return  books[counter++];
+
+        }
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new Iterator<>() {
+            private int counter = 0;
+
+            @Override
+            public boolean hasNext() {
+                return counter < books.length;
+            }
+
+            @Override
+            public Book next() {
+                return books[counter++];
+            }
+        };
+    }
+}
